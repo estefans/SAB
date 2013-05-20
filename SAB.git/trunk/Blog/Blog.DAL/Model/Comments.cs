d11c9959ccd9e0277ca607764c429dbf7565fd98
Ewelina <anastazja160489@buziaczek.pl>
2013-05-20 23:51:13 +0200

    zad

---
+++ SAB.git/trunk/Blog/Blog.DAL/Model/Comments.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.DAL.Model
+{
+    public class Comments
+    {
+        [Key]
+        public long PostId { get; set; }
+
+        public string Content { get; set; }
+    }
+}
